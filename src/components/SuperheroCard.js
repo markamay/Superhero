85c80b67d45c7be59e3921ba7d3ed1366e58f451
Mark May <markamay@live.com>
2021-10-05 18:23:29 -0400

    Added colors around superhero cards to denote alignment

--- src/components/SuperheroCard.js
+++ src/components/SuperheroCard.js
@@ -1,6 +1,16 @@
 const SuperheroCard = ({ hero }) => {
+    const hero_alignment = hero.biography.alignment;
+    let alignment_color;
+    if (hero_alignment === 'good') {
+        alignment_color = 'green';
+    } else if (hero_alignment ==='bad') {
+        alignment_color = 'red';
+    } else {
+        alignment_color = 'gray';
+    }
+
     return (
-        <div>
+        <div className="superhero-card-border" style={{backgroundColor: alignment_color}}>
             <div className="superhero-card">
                 <img src={hero.images.xs} alt={`${hero.name} thumbnail`}/>
                 <h3>{hero.name}</h3>
