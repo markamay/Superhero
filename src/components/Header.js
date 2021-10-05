764e27113e66924280bd76862271a589469acac5
Mark May <markamay@live.com>
2021-10-05 15:56:03 -0400

    Made header look slightly nicer

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ const Header = () => {
     return (
         <header class="header">
             <h1>Superheroes</h1>
+            <h2>Everyone needs a hero in their life.</h2>
         </header>
     )
 }
